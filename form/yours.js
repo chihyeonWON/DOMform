d04587a36e7ee13a90a7892086f7ab78f86f02b3
원치현 <heungeob1003@gmail.com>
2021-09-15 19:14:11 +0900

    Add selectors I think needed

---
+++ form/yours.js
@@ -0,0 +1,5 @@
+const dropdown = document.querySelector(".dropdown")
+const toggleButton = document.querySelector(".dropdown-toggle")
+const menu = document.querySelector(".dropdown-menu")
+const options = document.querySelectorAll(".dropdown-option")
+
