e6499b4959e829fa2cd53948031ad52fa2ded761
원치현 <heungeob1003@gmail.com>
2021-09-15 19:17:50 +0900

    Add eventlistener to toggleButton and toggle class show

--- form/yours.js
+++ form/yours.js
@@ -3,3 +3,9 @@ const toggleButton = document.querySelector(".dropdown-toggle")
 const menu = document.querySelector(".dropdown-menu")
 const options = document.querySelectorAll(".dropdown-option")
 
+// 토글버튼이 클릭 => 메뉴를 보여준다
+// 메뉴 max-height=0
+// 메뉴.show => 해지
+toggleButton.addEventListener("click",function(){
+    menu.classList.toggle("show")
+})
