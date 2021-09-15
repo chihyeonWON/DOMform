0534a6ed5b4bcfb8f2cb06c0bcb4256028a148a5
원치현 <heungeob1003@gmail.com>
2021-09-15 19:35:55 +0900

    Remove Attribute disabled in nextButton

--- form/yours.js
+++ form/yours.js
@@ -2,6 +2,7 @@ const dropdown = document.querySelector(".dropdown")
 const toggleButton = document.querySelector(".dropdown-toggle")
 const menu = document.querySelector(".dropdown-menu")
 const options = document.querySelectorAll(".dropdown-option")
+const nextButton = document.querySelector(".next-button")
 
 // 토글버튼이 클릭 => 메뉴를 보여준다
 // 메뉴 max-height=0
@@ -18,5 +19,6 @@ options.forEach(function(item) {
         const value = e.currentTarget.textContent.trim() //value 값에 공백을 없애주기 위해 trim을 설정
         toggleButton.textContent = value
         toggleButton.classList.add("selected")
+        nextButton.removeAttribute("disabled")
     })
 })
